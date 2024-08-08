98a6f36e5bfd9feaf697be3eaa41978d0b27911a
trevid <trevidmy@gmail.com>
2024-08-08 19:48:11 +0800

    3.7.0-1

---
+++ ports/fisco-bcos-cpp-sdk/3.7.0-1/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO zhaoxiaoxuannj/fiscotest
+        REF cb3671d0aa33a5d382e4111986c955f4f6db131a
+        SHA512 156691a24fd77f087ef4f552b7365fcc094fc9cf9943df337e301474f8c50a436e34c05db7303a11ac148568fbc66f0f3191d99f6fb377f41cf49daf86846324
+        HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+        -DALLOCATOR=default
+        -DTESTS=OFF
+        -DWITH_TIKV=OFF
+        -DWITH_TARS_SERVICES=OFF
+        -DWITH_LIGHTNODE=OFF
+        -DWITH_CPPSDK=ON
+        -DBUILD_STATIC=OFF
+        -DTOOLS=OFF
+        -DFULLNODE=OFF
+        -DWITH_WASM=OFF
+)
+
+vcpkg_cmake_build()
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"  RENAME copyright)
